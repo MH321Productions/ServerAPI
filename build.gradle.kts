4824cf200a9f59552d17b5747341006f5db0e230
Mika Herrmann <mikaherrmann1@gmx.net>
2024-11-17 23:04:46 +0100

    Setup shadow jar, change toolchain to 17, publish project in local maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    id("com.gradleup.shadow") version "8.3.5"
+    `maven-publish`
 }
 
 group = "io.github.MH321Productions"
@@ -20,15 +22,17 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
 
-    implementation("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
-    implementation("com.github.MilkBowl:VaultAPI:1.7")
-    implementation("net.luckperms:api:5.4")
-    implementation("com.comphenix.protocol:ProtocolLib:5.1.0")
+    compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("com.github.MilkBowl:VaultAPI:1.7")
+    compileOnly("net.luckperms:api:5.4")
+    compileOnly("com.comphenix.protocol:ProtocolLib:5.1.0")
 }
 
 tasks {
     processResources {
-        expand("version" to project.version)
+        expand(
+            "version" to project.version
+        )
 
         filteringCharset = "UTF-8"
     }
@@ -39,5 +43,17 @@ tasks {
 }
 
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(17)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = project.group.toString()
+            artifactId = project.name
+            version = project.version.toString()
+
+            from(components["kotlin"])
+        }
+    }
 }
